8c56ce5e6fb43c97fbc3162a7d951fba5064d6c4
joongwon <bjwlovekjh88>
2017-06-03 15:31:48 +0900

    Add Single Expression, Default Arguments, Name Arguments and vararg

--- src/_1_GettingStarted/DefiningFunction.kt
+++ src/_1_GettingStarted/DefiningFunction.kt
@@ -4,21 +4,50 @@ package _1_GettingStarted
  *  @author Leopold Baik
  *  https://github.com/joongwon
  */
-fun sum(a: Int, b: Int): Int {
+fun simple(a: Int, b: Int): Int {
     return a + b
 }
 
-fun sum2(a: Int, b: Int): Int = a + b
-
 fun printSum(a: Int, b: Int): Unit {
-    println("sum of $a and $b is ${a + b}")
+    println("simple of $a and $b is ${a + b}")
 }
 
 fun printSum2(a: Int, b: Int) {
-    println("sum of $a and $b is ${a + b}")
+    println("simple of $a and $b is ${a + b}")
 }
 
 infix fun Int.shl(a: Int): Int {
     return a + a
+}
+
+fun singleExpression(a: Int, b: Int): Int = a + b
+
+fun singleExpression2(a: Int, b: Int) = a + b // type inferred
+
+fun defaultArgs(a: Int = 10, b: Int = 10): Int {
+    return a + b
+}
+
+fun namedArgs(pre: String, post: String = "Hello", ignore: Boolean = true): String {
+    if (ignore) {
+        return pre
+    }
+
+    return pre + post
+}
+
+fun <T> asList(vararg args: T): List<T> {
+    val result = ArrayList<T>()
+    for (a in args)
+        result.add(a)
+    return result
+}
+
+fun <T> asList2(pre: T, vararg args: T): List<T> {
+    val result = ArrayList<T>()
+    result.add(pre)
+    for (a in args)
+        result.add(a)
+    return result
 }
 
--- test/_1_GettingStarted/DefiningFunctionTest.kt
+++ test/_1_GettingStarted/DefiningFunctionTest.kt
@@ -9,12 +9,8 @@ import org.junit.Test
  */
 class DefiningTest {
 
-    @Test fun testSum() {
-        assertEquals(12, sum(10, 2))
-    }
-
-    @Test fun testSum2() {
-        assertEquals(10, sum2(5, 5))
+    @Test fun testSimple() {
+        assertEquals(12, simple(10, 2))
     }
 
     @Test fun testInfix() {
@@ -23,5 +19,39 @@ class DefiningTest {
 
     @Test fun testInfix2() {
         assertEquals(4, 1.shl(2))
+    }
+
+    @Test fun singleExpression() {
+        assertEquals(10, singleExpression(5, 5))
+    }
+
+    @Test fun singleExpression2() {
+        assertEquals(10, singleExpression2(5, 5))
+    }
+
+    @Test fun testDefaultArgs() {
+        assertEquals(20, defaultArgs())
+    }
+
+    @Test fun testNamedArgs() {
+        assertEquals("Say", namedArgs(pre = "Say"))
+    }
+
+    @Test fun testNamedArgs2() {
+        assertEquals("SayHello", namedArgs(pre = "Say", post = "Hello", ignore = false))
+    }
+
+    @Test fun testNamedArgs3() {
+        assertEquals("Say", namedArgs("Say", "Hello"))
+    }
+
+    @Test fun testVarArg() {
+        val list = asList(1, 2, 3)
+        assertEquals(3, list.size)
+    }
+
+    @Test fun testVarArg2() {
+        val list = asList2(10, 1, 2, 3)
+        assertEquals(4, list.size)
     }
 }
