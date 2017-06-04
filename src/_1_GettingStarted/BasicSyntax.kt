ea217c02e0d2c01f60567f82d970ef776b3fba16
joongwon <bjwlovekjh88>
2017-06-04 17:45:51 +0900

    Add when expressions

--- src/_1_GettingStarted/BasicSyntax.kt
+++ src/_1_GettingStarted/BasicSyntax.kt
@@ -38,7 +38,7 @@ fun stringTemplates() {
     println(s2)
 }
 
-fun conditionalExpression(a: Int, b: Int): Int {
+fun ifExpression(a: Int, b: Int): Int {
     if (a > b) {
         return a
     } else {
@@ -46,6 +46,42 @@ fun conditionalExpression(a: Int, b: Int): Int {
     }
 }
 
-fun ifExpression(a: Int, b: Int): Int = if (a > b) a else b
+fun ifExpression2(a: Int, b: Int): Int = if (a > b) a else b
+
+fun whenExpression(x: Int): String {
+    when(x) {
+        1 -> return "Hi"
+        2 -> return "Good"
+        else -> return "Sad"
+    }
+}
+
+fun whenExpression2(x: Int): String {
+    when(x) {
+        1, 2, 3 -> return "Yes"
+        else -> return "No"
+    }
+}
+
+fun whenExpression3(x: Int): String {
+    when(x) {
+        in 1..10 -> return "In"
+        !in 10..20 -> return "Outside"
+        else -> return "None"
+    }
+}
+
+fun whenExpression4(x: Any): Any {
+    when(x) {
+        in 1..10 -> return x
+        "Say" -> return "Hello"
+        else -> return "NotFound"
+    }
+}
+
+fun whenExpression5(x: Any) = when(x) {
+    is String -> x.startsWith("prefix")
+    else -> false
+}
 
 
--- test/_1_GettingStarted/BasicSyntaxTest.kt
+++ test/_1_GettingStarted/BasicSyntaxTest.kt
@@ -9,12 +9,42 @@ import org.junit.Test
  */
 class BasicSyntaxTest {
 
-    @Test fun conditionalExpressionTest() {
-        assertEquals(3, conditionalExpression(3, 1))
-    }
-
     @Test fun ifExpressionTest() {
         assertEquals(3, ifExpression(3, 1))
+    }
+
+    @Test fun ifExpression2Test() {
+        assertEquals(3, ifExpression2(3, 1))
+    }
+
+    @Test fun whenExpressionTest() {
+        assertEquals("Hi", whenExpression(1))
+        assertEquals("Good", whenExpression(2))
+        assertEquals("Sad", whenExpression(10))
+    }
+
+    @Test fun whenExpression2Test() {
+        assertEquals("Yes", whenExpression2(1))
+        assertEquals("Yes", whenExpression2(2))
+        assertEquals("Yes", whenExpression2(3))
+        assertEquals("No", whenExpression2(10))
+    }
+
+    @Test fun whenExpression3Test() {
+        assertEquals("In", whenExpression3(9))
+        assertEquals("Outside", whenExpression3(21))
+        assertEquals("None", whenExpression3(15))
+    }
+
+    @Test fun whenExpression4Test() {
+        assertEquals("Hello", whenExpression4("Say"))
+        assertEquals(5, whenExpression4(5))
+        assertEquals("NotFound", whenExpression4(""))
+    }
+
+    @Test fun whenExpression5Test() {
+        assertEquals(true, whenExpression5("prefix"))
+        assertEquals(false, whenExpression5("empty"))
     }
 }
 
