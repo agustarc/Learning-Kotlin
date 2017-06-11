33117b8d9dbd9f202e7c1ae5ed974237168fdfe1
joongwon <joongwon100@gmail.com>
2017-06-11 16:01:39 +0900

    Add abstract class sample code

---
+++ src/_3_ClassesAndObjects/AbstractClass.kt
@@ -0,0 +1,17 @@
+package _3_ClassesAndObjects
+
+/**
+ *  @author Leopold Baik
+ *  https://github.com/joongwon
+ */
+open class NonAbstract {
+    open fun call() {}
+}
+
+// A class and some of its members may be declared abstract.
+// An abstract member does not have an implementation in its class.
+// Note that we do not need to annotate an abstract class or function with open – it goes without saying.
+// We can override a non-abstract open member with an abstract one
+abstract class AbstractClass : NonAbstract() {
+    override abstract fun call()
+}
